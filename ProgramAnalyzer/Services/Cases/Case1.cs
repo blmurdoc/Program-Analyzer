d7805bf23821133c352d92bbab09fd55722f351d
blmurdoc <blake.murdock@ttu.edu>
2014-04-04 14:44:41 -0500

    Basic Refactor
    Nothing special

--- ProgramAnalyzer/Services/Cases/Case1.cs
+++ ProgramAnalyzer/Services/Cases/Case1.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Services.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Services
 {
+    [DataContract]
     public class Case1
     {
+        [DataMember]
+        public List<UnevaluatedObject> ContainedSemiSecurityObjects { get; set; }
     }
 }
