d3dab4d4ca969e8eb7be69ed9dd7ce5c65d87705
blmurdoc <blake.murdock@ttu.edu>
2014-04-23 00:16:20 -0500

    Global

---
+++ ProgramAnalyzer/Services/Managers/Global.cs
@@ -0,0 +1,30 @@
+﻿using Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Managers
+{
+    [DataContract]
+    public class Global
+    {
+        [DataMember]
+        public List<UnevaluatedObject> UnevaluatedObjects
+        {
+            get
+            {
+                if (unevaluatedObjects == null)
+                    unevaluatedObjects = new List<UnevaluatedObject>();
+                return unevaluatedObjects;
+            }
+            set
+            {
+                unevaluatedObjects = value;
+            }
+        }
+        private List<UnevaluatedObject> unevaluatedObjects;
+    }
+}
